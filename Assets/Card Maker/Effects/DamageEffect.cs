783e84872c982b113d1cac9dd99a40417c95b51d
Norbert Gonczol <ngonczol@gmail.com>
2024-06-25 14:21:42 +0100

    fixed damage effect

--- Assets/Card Maker/Effects/DamageEffect.cs
+++ Assets/Card Maker/Effects/DamageEffect.cs
@@ -5,5 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "DamageEffect", menuName = "CardEffects/DamageEffect", order = 1)]
 public class DamageEffect : Effect
 {
-
+    public override void DoEffect()
+    {
+        
+    }
 }

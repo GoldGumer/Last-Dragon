b1c6befcc4df853d20104cc8738919e983510c1a
Norbert Gonczol <ngonczol@gmail.com>
2024-06-25 14:18:36 +0100

    setup effects and card making process

---
+++ Assets/Card Maker/Effects/DamageEffect.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DamageEffect", menuName = "CardEffects/DamageEffect", order = 1)]
+public class DamageEffect : Effect
+{
+
+}

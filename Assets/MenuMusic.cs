6b8e574faf112d1d2170a08b5fb043479b8407b6
KazThomas <mr.firebass@gmail.com>
2024-06-27 23:44:13 +0100

    more music nonsense, main doesnt flow into instruct

---
+++ Assets/MenuMusic.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuMusic : MonoBehaviour
+{
+    [SerializeField] private AudioSource music;
+    [SerializeField] private GameObject musicPlayer; 
+
+    private void Awake()
+    {
+        string index = SceneManager.GetActiveScene().name;
+        if (index == "Main_Menu" )
+        {
+            DontDestroyOnLoad(transform.gameObject);
+            
+            music = GetComponent<AudioSource>();
+        }
+        if (index == "HexGrid")
+        {
+           musicPlayer.SetActive(false);
+            music.Stop();
+        }
+    }
+
+    private void PlayMusic()
+    {
+        if (music.isPlaying)
+        {
+            return;
+        }
+        else music.Play();
+    }
+}
